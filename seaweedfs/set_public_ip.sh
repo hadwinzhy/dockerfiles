40c9131bd98d1cfb8d07b4bb9e55c74eb327026d
yckj2289 <zhenghuiyun@cloudwalk.cn>
2019-09-30 15:22:39 +0800

    chang export to source

--- seaweedfs/set_public_ip.sh
+++ seaweedfs/set_public_ip.sh
@@ -4,6 +4,6 @@ file_path='/var/seaweedfs_config/public_ip_address'
 
 if [ -f "$file_path" ]
 then
-  export PUBLIC_IP_ADDRESS=`cat "$file_path"`
+  source "$file_path"
 fi
 echo "public ip is: $PUBLIC_IP_ADDRESS"
